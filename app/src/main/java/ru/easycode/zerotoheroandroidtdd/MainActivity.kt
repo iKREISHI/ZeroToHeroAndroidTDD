a76a2671b968bcc065daf0017a98c44e0ae224e8
shiro-av <vadas25@yandex.ru>
2024-02-23 18:58:23 +0500

    task 004-1 solved (layout, textView and button in MainActivity)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,25 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        val linearLayout = LinearLayout(this)
+        val textView = TextView(this)
+        textView.id = R.id.titleTextView
+        textView.text = "I am an Android Developer!"
+
+        val button = Button(this)
+        button.text = "Click me"
+        button.height = 70
+
+        linearLayout.addView(textView)
+        linearLayout.addView(button)
+        setContentView(linearLayout)
     }
 }
