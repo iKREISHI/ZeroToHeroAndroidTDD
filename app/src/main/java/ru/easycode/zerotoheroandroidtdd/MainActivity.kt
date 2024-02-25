73ccf7836505c4c77b3aeacc7267f4fc31ce9906
shiro-av <vadas25@yandex.ru>
2024-02-25 19:58:56 +0500

    task 006 change text runtime solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,26 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.changeButton)
+        val textView = findViewById<TextView>(R.id.titleTextView)
+//        button.setOnClickListener {
+//            textView.text = "I am an Android Developer!"
+//        }
+        button.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                textView.text = "I am an Android Developer!"
+            }
+        })
     }
 }
