338db697dbbde91ae97eedbc264077c7c8deced2
shiro-av <vadas25@yandex.ru>
2024-02-26 08:36:19 +0500

    task 007 keep text using Activity lifecycle solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,17 +2,34 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.titleTextView)
+        textView = findViewById<TextView>(R.id.titleTextView)
         val button = findViewById<Button>(R.id.changeButton)
         button.setOnClickListener {
             textView.text = "I am an Android Developer!"
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("myStr", textView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState )
+        textView.text = savedInstanceState.getString("myStr")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
