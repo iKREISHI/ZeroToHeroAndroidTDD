d62d922e37d6407fd3afc39c6d2777ab94f0a3cc
shiro-kreishi <vadas25@yandex.ru>
2024-03-20 23:32:32 +0500

    task 012 increment max solved fix Count

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -16,18 +16,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.incrementButton)
         textView = findViewById<TextView>(R.id.countTextView)
-
+        val count: Count = Count.Base(step = 2)
         savedInstanceState?.let {
-            textView.text = savedInstanceState.getString("increment")
+            textView.text = savedInstanceState.getString(TEXT_KEY)
         }
 
         button.setOnClickListener() {
-            textView.text = (textView.text.toString().toInt() + 2).toString()
+            textView.text = count.increment(textView.text.toString())
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString("increment", textView.text.toString())
+        outState.putString(TEXT_KEY, textView.text.toString())
+    }
+
+    companion object {
+        private const val TEXT_KEY = "increment"
     }
 }
